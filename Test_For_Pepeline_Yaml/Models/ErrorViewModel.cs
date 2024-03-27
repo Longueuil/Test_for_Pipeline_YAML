335ef459594a2c4901163739510485649e4eccbe
Vladimir <mt@mts-macbook-air.home>
2024-03-27 17:49:19 -0400

    init commit

---
+++ Test_For_Pepeline_Yaml/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Test_For_Pepeline_Yaml.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
